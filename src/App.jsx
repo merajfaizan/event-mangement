70d064217c624e24a631f5c458dd5ab79bf3d140
Meraj Faizan <mdmaraj1500@gmail.com>
2023-10-06 22:22:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-5xl capitalize">
+        event management website setup
+      </h1>
+    </div>
+  );
+}
+
+export default App;
